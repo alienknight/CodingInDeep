a072b3a348ba0ce0e4f44ead525f502d14b42afc
alienknight <reality.st73@gmail.com>
2014-11-28 15:29:01 -0800

    Update MedianofTwoSortedArray.java

--- leetcode/MedianofTwoSortedArray.java
+++ leetcode/MedianofTwoSortedArray.java
@@ -1,3 +1,33 @@
+public class Solution {
+    public double findMedianSortedArrays(int A[], int B[]) {
+        if (A == null || B == null || A.length == 0 && B.length == 0)
+            return 0.00;
+        int m = A.length, n = B.length;
+        if ((m + n) % 2 == 1) {
+            return findMedianSortedArrays(A, 0, m, B, 0, n, (m + n) / 2 + 1);
+        } else {
+            return (findMedianSortedArrays(A, 0, m, B, 0, n, (m + n) / 2 + 1) + findMedianSortedArrays(A, 0, m, B, 0, n, (m + n) / 2)) / 2;
+        }
+    }
+    
+    public double findMedianSortedArrays(int A[], int startA, int endA, int B[], int startB, int endB, int k) {
+        if (endA - startA > endB - startB)  return findMedianSortedArrays(B, startB, endB, A, startA, endA, k);
+        else if(endA <= startA) return B[startB + k -1];
+        else if(k==1) return Math.min(A[startA], B[startB]);
+        
+        int pa = Math.min(endA - startA, k / 2), pb = k - pa;
+        if (A[startA + pa - 1] < B[startB + pb - 1])
+            return findMedianSortedArrays(A, startA + pa, endA, B, startB, endB, k - pa);
+        else if (A[startA + pa - 1] > B[startB + pb - 1])
+            return findMedianSortedArrays(A, startA, endA, B, startB + pb, endB, k - pb);
+        else
+            return A[startA + pa -1];
+    }
+}
+
+
+
+
 
 public class MedianofTwoSortedArray {
 	public double findMedianSortedArrays(int A[], int B[]) {
